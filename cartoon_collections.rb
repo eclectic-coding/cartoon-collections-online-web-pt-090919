789afbdb2560969afe7308758d8d6c483d519286
eclecticcoding <chuck@eclecticsaddlebag.com>
2019-09-11 13:24:44 -0400

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,31 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(dwarves)
+  i = 0
+
+  new_arr = []
+  while i < dwarves.length
+    new_arr << dwarves.each_with_index do |value, index|
+      puts "#{index + 1}. #{value}"
+    end
+    i += 1
+  end
+  new_arr
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(planeteer_calls)
+  planeteer_calls.map { |call| call.capitalize + '!' }
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(array)
+  array.any? { |word| word.length > 4 }
 end
 
-def find_the_cheese# code an argument here
+item_list = %w[umbrella spinach cheddar helicopter]
+def find_the_cheese(item_list)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  item_list.find do |maybe_okay|
+    cheese_types.include?(maybe_okay)
+  end
 end
+
+find_the_cheese(item_list)
